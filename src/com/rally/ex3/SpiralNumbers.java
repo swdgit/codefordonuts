6e0e37c7f238db379fed165043e3d2cb5bdc0e52
swdgit <stacy.decker@gmail.com>
2014-01-24 01:59:09 -0700

    This is where I am. Bugs and all. Need to work out some more kinks.

--- src/com/rally/ex3/SpiralNumbers.java
+++ src/com/rally/ex3/SpiralNumbers.java
@@ -3,6 +3,9 @@
  */
 package com.rally.ex3;
 
+import java.util.ArrayList;
+
+
 /**
  * @author swdmac
  *
@@ -13,7 +16,7 @@ public class SpiralNumbers {
      * default constructor that isn't used.
      */
     public SpiralNumbers() {
-    
+        
     }
 
     /**
@@ -22,40 +25,185 @@ public class SpiralNumbers {
      * @param spiralNumber
      */
     public SpiralNumbers(Integer spiralNumber) {
+
+        // determine the corner values as the spiral progresses away from zero.
+        ArrayList<Integer> corners = new ArrayList<Integer>();
+        
+        int corner = -1;
+        int currentCorner = 0;
+        while (spiralNumber >= currentCorner) {
+            currentCorner = new Integer(corner * (corner + 1));
+            corners.add(currentCorner);
+            corner += 2;
+        }
+
+        for (Integer integer : corners) {
+            System.out.print(" " + integer.toString());
+        }
+
+        // leverage these corners to fill out the array
+        
+        System.out.println("");
+        
+        
+        
+        
+        
+        
         // how many rows and columns will I need.
         // square root of the spiralNumber gets us close
         double sqr = Math.sqrt(spiralNumber);
         
-        // push the number up to the next integer
+        // push the number up to the next integer so the grid is big enough
         int xy = (int)Math.ceil(sqr);
-        
-        
-        int rowsAndColumns[][] = new int[xy][xy];
-
-        // We've got a grid, now we need to put some numbers in it.
-        // What's my center? 
-        
-        int centerIsh = Math.round(xy / 2);
-        System.out.println(centerIsh);
-        
-        rowsAndColumns[centerIsh][centerIsh] = 0;
-        
-        for (int i = 0; i < spiralNumber; i++) {
-            
-            
-            
-            
+        if ((xy % 2) == 0) {
+            xy++;
         }
         
-        // done... Now show off our results.
-        System.out.println(xy);
+              
+        System.out.println("grid size : " + xy + " : " + xy);
+        String rowsAndColumns[][] = new String[xy][xy];
+        
+        // start point is zero based on the array.
+        int start = Math.round(xy/2) - 1; 
+        System.out.println("Starting at : " + start);
+
+        try {
+            boolean rowUp   = false;
+            boolean rowDown = false;
+            boolean colBack = false;
+            boolean colFwd  = true;
+            
+            int row = start;
+            int col = start;
+            int lastCorner = 0;
+            int segment    = -1; // which corner segment are we on.
+            int currentSegment = segment;
+            int segmentLeg = 1;
+            
+            for (Integer c : corners) {
+                rowsAndColumns[start][start] = c.toString();
+                rowUp   = true;
+                rowDown = false;
+                colBack = false;
+                colFwd  = false;
+
+                // fill in the other numbers based on each corner           
+                for (int a = c.intValue(); a >= lastCorner ; a--) {
+                    // location, location, location...
+                    // I know what my starting point is. 
+                    // I need to cycle around that till I fill in all the numbers for this corner
+                    //
+                    System.out.print(a + " ");
+    
+                    // need to set position of row and col here as we cycle backwards
+                    // to the lastCorner.
+                    if (a <= spiralNumber) {
+                        rowsAndColumns[row][col] = String.valueOf(a);
+                    } else {
+                        rowsAndColumns[row][col] = "";                       
+                    }
+                    
+                    if (rowUp) {
+                        row--;
+                        if (currentSegment <= segmentLeg) {
+                            rowUp   = false;
+                            colBack = true;
+                            
+                            currentSegment = segment;
+                            segmentLeg = 1;
+                            continue;
+                        }
+                        
+                    }
+                    
+                    if (colBack) {
+                        col--;
+                        if (currentSegment <= segmentLeg) {
+                            colBack = false;
+                            rowDown = true;
+
+                            currentSegment = segment - 1;
+                            segmentLeg = 1;
+                            continue;
+                        }
+                    }
+                    
+                    if (rowDown) {
+                        row++;
+                        if (currentSegment <= segmentLeg) {
+                            rowDown = false;
+                            colFwd  = true;
+
+                            currentSegment = segment - 1;
+                            segmentLeg = 1;
+                            continue;
+                        }
+                    }
+                    
+                    if (colFwd) {
+                        col++;
+                        if (currentSegment <= segmentLeg) {
+                            colFwd = false;
+                            rowUp  = true;
+
+                            currentSegment = segment - 2;
+                            segmentLeg = 1;
+                            continue;
+                        }
+                    }
+                    
+                    segmentLeg++;
+                }
+                segment += 2;
+                currentSegment = segment;
+                lastCorner = c.intValue();
+                start++;
+                row = start;
+                col = start;
+
+                System.out.println(" segment :  " + segment);
+            }
+        } catch (Exception e) {
+            System.out.println("Wa Happen :: " + e.getMessage());
+        }
+        
+//        int r = 0;
+//        int c = 0;
+//        for (int i = 0; i <= spiralNumber; i++) {
+//            rowsAndColumns[r][c] = String.valueOf(i);
+//            
+//            start++;
+//        }
+                
+        // top left bottom and right are the min and max of rows from 0 to xy.        
+        // 
+        
+        // 0 1 2 3 4 5 6 7 8 9 10 11 12 13
+        // ok done with the spiral and we can print it out.
+        for (int r = 0; r < xy; r++) {
+            for (int c = 0; c < xy; c++) {
+                System.out.print(padLeft(rowsAndColumns[r][c], 4));
+            }
+            System.out.println("");
+           
+        }
+    }
+    
+    /**
+     * @param s value to pad
+     * @param n how much padding
+     * @return padded value
+     */
+    private String padLeft(String s, int n) {
+        return String.format("%1$" + n + "s", s);
     }
     
     /**
      * @param args
      */
     public static void main(String[] args) {
-        
+       
         boolean printUsage = true;
         
         // check for an arg.
@@ -79,9 +227,13 @@ public class SpiralNumbers {
             //no arg print usage
             // usage -- enter an integer (number) to get a spiral output.
             // invalid input as well.
+            System.out.println("*****************");
+            System.out.println("*****************");
             System.out.println("This tiny little app will print a good Spiral if you provide a number.");
             System.out.println("A number between 4 and 2,500 will be safe for most monitors.");
+            System.out.println("*****************");
+            System.out.println("*****************");
         }
-        
     }
+        
 }
